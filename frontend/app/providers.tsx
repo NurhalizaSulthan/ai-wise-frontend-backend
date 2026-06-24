57ac872c50286640a77411035720c779888d6e15
Nurfadillah Umar <itsennuu@gmail.com>
2026-06-24 21:24:25 +0800

    refactor: update provider configuration

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import React from "react";
 import { ThemeProvider } from "next-themes";
 
-const Providers = ({
-    children,
-}: {
-    children: React.ReactNode;
-}) => {
+const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={false}
+            disableTransitionOnChange
+            storageKey="ai-wise-theme"
+        >
             {children}
         </ThemeProvider>
     );
-}
+};
 
 export default Providers;
